15f2ab70dfce84d8d6e8ae52eed6a068b709002c
Robert Yamasaki <rhy2781@rit.edu>
2024-03-26 20:45:54 -0400

    implement updated view based on cached data

--- src/components/ArtistView/ArtistView.js
+++ src/components/ArtistView/ArtistView.js
@@ -7,21 +7,20 @@ import { IoPlaySharp } from "react-icons/io5";
 
 function ArtistView(props) {
 
-    const [data, setData] = useState({})
+    const [artistData, setArtistData] = useState({})
+    const [trackData, setTrackData] = useState([])
+
+
     const [tracks, setTracks] = useState([])
     const [showAll, setShowAll] = useState(false)
     const [hoverTrack, setHoverTrack] = useState(11)
 
 
-    const handleMouseEnter = (index) => {
-        setHoverTrack(index)
-        console.log(index)
-    }
-    const handleMouseLeave = () => {
-        setHoverTrack(null)
-        console.log("mouse leave")
-    }
+    const handleMouseEnter = (index) => { setHoverTrack(index) }
+    const handleMouseLeave = () => { setHoverTrack(null) }
 
+
+    // get base data for artist
     useEffect(() => {
         const getData = async () => {
             await fetch(`${process.env.REACT_APP_BACKEND}/artist?id=${props.spotifyId}`, {
@@ -29,52 +28,56 @@ function ArtistView(props) {
             })
                 .then((response) => response.json())
                 .then((response) => {
-                    console.log(response)
-                    setData(response)
+                    setArtistData(response)
                 })
         }
         getData()
+    }, [props.spotifyId])
 
+    // get new data for artist top tracks
+    useEffect(() => {
         const getTopTracks = async () => {
             await fetch(`${process.env.REACT_APP_BACKEND}/artist/tracks?id=${props.spotifyId}`, {
                 method: 'GET',
             })
                 .then((response) => response.json())
                 .then((response) => {
-                    console.log("tracks")
-                    console.log(response)
-                    const view = response.tracks.map((element, index) => {
-                        const min = Math.floor((element.duration_ms / 1000) / 60)
-                        const sec = Math.floor((element.duration_ms / 1000) % 60).toString().padStart(2, '0')
-                        return (
-                            <div className="TopTrack" key={index} onMouseEnter={() => handleMouseEnter(index)} onMouseLeave={handleMouseLeave}>
-                                <div className="TopTrackNumber">
-                                    {(hoverTrack !== index) ? index + 1 : <IoPlaySharp />}
-                                </div>
-                                <div className="TopTrackImage">
-                                    <div className="TopTrackImageContainer">
-                                        <img src={element.album.images[0].url} alt={element.uri} />
-                                    </div>
-                                </div>
-                                <div className="TopTrackName">
-                                    {element.name}
-                                </div>
-                                <div className="TopTrackTime">
-                                    {min}:{sec}
-                                </div>
-                            </div>
-                        )
-                    })
-                    setTracks(view)
+                    setTrackData(response.tracks)
+                    console.log(response.tracks)
                 })
         }
         getTopTracks()
 
-    }, [props.spotifyId, hoverTrack])
+    }, [props.spotifyId])
 
+    // visually update with cached data, and state update with hover variable
     useEffect(() => {
+        const visual = trackData.map((element, index) => {
+            const min = Math.floor((element.duration_ms / 1000) / 60)
+            const sec = Math.floor((element.duration_ms / 1000) % 60).toString().padStart(2, '0')
 
-    }, [showAll, hoverTrack])
+            return (
+                <div className="TopTrack" key={index} onMouseEnter={() => handleMouseEnter(index)} onMouseLeave={handleMouseLeave}>
+                    <div className="TopTrackNumber">
+                        {(hoverTrack !== index) ? index + 1 : <IoPlaySharp />}
+                    </div>
+                    <div className="TopTrackImage">
+                        <div className="TopTrackImageContainer">
+                            <img src={element.album.images[0].url} alt={element.uri} />
+                        </div>
+                    </div>
+                    <div className="TopTrackName">
+                        {element.name}
+                    </div>
+                    <div className="TopTrackTime">
+                        {min}:{sec}
+                    </div>
+                </div>
+            )
+        })
+
+        setTracks(visual)
+    }, [trackData, hoverTrack])
 
     const handleShow = () => {
         if (showAll) { setShowAll(false) }
@@ -83,18 +86,18 @@ function ArtistView(props) {
 
     return (
         <div className="ArtistView">
-            {(Object.keys(data).length > 0 && Object.keys(tracks).length > 0) &&
+            {(Object.keys(artistData).length > 0 && Object.keys(tracks).length > 0) &&
                 <div>
                     <div className="ArtistHeader">
                         <div className="ArtistImage">
-                            <img src={data.images[0].url} />
+                            <img src={artistData.images[0].url} />
                         </div>
                         <div className="ArtistDetails">
                             <div className="ArtistName">
-                                {data.name}
+                                {artistData.name}
                             </div>
                             <div>
-                                {data.followers.toLocaleString()} Followers
+                                {artistData.followers.toLocaleString()} Followers
                             </div>
                         </div>
                     </div>

8c75239a716a5e2c7559a24556b065a4fae5d789
Robert Yamasaki <rhy2781@rit.edu>
2024-03-27 14:32:40 -0400

    fixed current playing track component css

--- src/components/current/Current.js
+++ src/components/current/Current.js
@@ -7,21 +7,18 @@ function Current(props) {
     const [artists, setArtists] = useState([])
 
     useEffect(() => {
-        console.log(props.track.artists)
         const temp = props.track.artists.map((artist, index, arr) => (
             < div key={index} className="Artist" onClick={() => { props.addPage(artist.uri) }}>
                 {(index === arr.length - 1) ? ` ${artist.name}` : `${artist.name}, `}
             </div >
         ));
-
         setArtists(temp);
 
     }, [props.track])
 
     return (
-        <div>
-            This is the current container
-            {/* <div className="CurrentImageContainer">
+        <div className="CurrentContainer">
+            <div className="CurrentImageContainer">
                 <img
                     src={props.track.album.images[0].url}
                     alt={props.track.uri}
@@ -34,7 +31,7 @@ function Current(props) {
                 <div className="CurrentArtists">
                     {artists}
                 </div>
-            </div> */}
+            </div>
         </div>
     )
 }

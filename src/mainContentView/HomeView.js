4a2271f92c7f7bff7b797f03d423da862c3b2b60
Robert Yamasaki <rhy2781@rit.edu>
2024-05-01 14:59:53 -0700

    upgrading react-scripts to version 5.0.1 to utilize mui-x/charts

--- src/mainContentView/HomeView.js
+++ src/mainContentView/HomeView.js
@@ -2,6 +2,9 @@ import { React, useEffect, useState } from "react";
 
 import DisplayRow from "../components/DisplayRow";
 import TrackList from "../components/TrackList";
+import Button from '@mui/material/Button';
+import { BarChart } from "@mui/x-charts";
+import { PieChart } from "@mui/x-charts/PieChart";
 
 function HomeView(props) {
 
@@ -34,6 +37,33 @@ function HomeView(props) {
                 submitRequest={props.submitRequest}
                 circle={true}
             />
+                  <Button variant="text">Text</Button>
+                  <BarChart
+                series={[
+                    { data: [35, 44, 24, 34] },
+                    { data: [51, 6, 49, 30] },
+                    { data: [15, 25, 30, 50] },
+                    { data: [60, 50, 15, 25] },
+                ]}
+                height={290}
+                xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
+                margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
+                />
+            <PieChart
+                series={[
+                    {
+                    data: [
+                        { id: 0, value: 10, label: 'series A' },
+                        { id: 1, value: 15, label: 'series B' },
+                        { id: 2, value: 20, label: 'series C' },
+                    ],
+                    },
+                ]}
+                width={400}
+                height={200}
+                />
+
+
             <TrackList
                 height={70}
                 padding={5}
